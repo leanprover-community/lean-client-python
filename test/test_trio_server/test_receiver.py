6547bedbb606fec95a8d62d3f81bf7e4e99d4890
Jason Rute <jason.rute@gmail.com>
2020-04-26 08:57:56 -0400

    switch to pytest

--- test/test_trio_server/test_receiver.py
+++ test/test_trio_server/test_receiver.py
@@ -1,4 +1,3 @@
-import unittest
 from test.test_trio_server.mock_lean import \
     LeanShouldGetRequest, LeanSendsBytes, LeanSendsResponse, LeanShouldNotGetRequest, LeanTakesTime, start_with_mock_lean
 from lean_client.trio_server import TrioLeanServer
@@ -6,45 +5,40 @@ import trio
 import trio.testing
 
 
-class TestReciever(unittest.TestCase):
-    def test_reciever_processes_only_whole_messages(self):
-        """
-        Lean sometimes will not send a complet message over at a time.  It may even break up a unicode character
-        like "⊢" (three bytes).
+def test_reciever_processes_only_whole_messages():
+    """
+    Lean sometimes will not send a complet message over at a time.  It may even break up a unicode character
+    like "⊢" (three bytes).
 
-        This tests that the message is properly received.
-        """
+    This tests that the message is properly received.
+    """
 
-        expected_state = "⊢ true"  # In bytes this is b'\xe2\x8a\xa2 true'
+    expected_state = "⊢ true"  # In bytes this is b'\xe2\x8a\xa2 true'
 
-        mock_lean_script = [
-            # initial sync
-            LeanShouldGetRequest({"file_name": "test.lean", "content": 'example : true := \nbegin end', "seq_num": 1, "command": "sync"}),
-            LeanSendsResponse({"message": "file invalidated", "response": "ok", "seq_num": 1}),
-            LeanTakesTime(.01),
-            LeanSendsResponse({"is_running": False, "response": "current_tasks", "tasks": []}),
-            LeanShouldGetRequest({"file_name": "test.lean", "line": 2, "column": 0, "seq_num": 2, "command": "info"}),
+    mock_lean_script = [
+        # initial sync
+        LeanShouldGetRequest({"file_name": "test.lean", "content": 'example : true := \nbegin end', "seq_num": 1, "command": "sync"}),
+        LeanSendsResponse({"message": "file invalidated", "response": "ok", "seq_num": 1}),
+        LeanTakesTime(.01),
+        LeanSendsResponse({"is_running": False, "response": "current_tasks", "tasks": []}),
+        LeanShouldGetRequest({"file_name": "test.lean", "line": 2, "column": 0, "seq_num": 2, "command": "info"}),
 
-            # response sent in two chunks over the stream splitting the "⊢" character b'\xe2\x8a\xa2' in half.
-            LeanSendsBytes(b'{"record":{"state":"\xe2\x8a'),
-            LeanTakesTime(.01),
-            LeanSendsBytes(b'\xa2 true"},"response":"ok","seq_num":2}\n'),
-        ]
+        # response sent in two chunks over the stream splitting the "⊢" character b'\xe2\x8a\xa2' in half.
+        LeanSendsBytes(b'{"record":{"state":"\xe2\x8a'),
+        LeanTakesTime(.01),
+        LeanSendsBytes(b'\xa2 true"},"response":"ok","seq_num":2}\n'),
+    ]
 
-        async def check_waiting_behavior():
-            async with trio.open_nursery() as nursery:
-                server = TrioLeanServer(nursery, debug_bytes=True)
-                await start_with_mock_lean(server, mock_lean_script)
+    async def check_behavior():
+        async with trio.open_nursery() as nursery:
+            server = TrioLeanServer(nursery, debug_bytes=True)
+            await start_with_mock_lean(server, mock_lean_script)
 
-                await server.full_sync('test.lean', content='example : true := \nbegin end')
-                state = await server.state(filename="test.lean", line=2, col=0)
+            await server.full_sync('test.lean', content='example : true := \nbegin end')
+            state = await server.state(filename="test.lean", line=2, col=0)
 
-                assert state == "⊢ true"
+            assert state == "⊢ true"
 
-                nursery.cancel_scope.cancel()
+            nursery.cancel_scope.cancel()
 
-        trio.run(check_waiting_behavior)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    trio.run(check_behavior)
